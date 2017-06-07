ecbc85b442f9ce8ddf43c0959c3974f4c654fb19
Thierry Schouten <s1079134@student.hsleiden.nl>
2017-06-07 11:16:46 +0200

    Youtube filmpjes worden nu getoond in de app.

---
+++ Fyxio/app/src/main/java/ipmedt4/example/com/fyxio/Config.java
@@ -0,0 +1,14 @@
+package ipmedt4.example.com.fyxio;
+
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyC7BBVjvp1pei8YGfbmLlw_YTfiHXin5Ec";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO1_CODE = "mEcoza72OWc";
+    //public static final String YOUTUBE_VIDEO1_CODE = "_oEA18Y8gM0";
+    public static final String YOUTUBE_VIDEO2_CODE = "KKrdfW8JOHg";
+    public static final String YOUTUBE_VIDEO3_CODE = "jJhbAq2epNA";
+
+}
