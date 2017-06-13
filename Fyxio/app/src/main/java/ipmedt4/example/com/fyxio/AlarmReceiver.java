ff2cdffe24d3640645a59db3ab0ed6d5715c3902
edoglaz <edoglaz@yahoo.com>
2017-06-13 15:30:33 +0200

    Notificatie toegevoegd
    Er kunnen nu notificaties ingesteld worden door de gebruiker.

---
+++ Fyxio/app/src/main/java/ipmedt4/example/com/fyxio/AlarmReceiver.java
@@ -0,0 +1,33 @@
+package ipmedt4.example.com.fyxio;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent repeating_intent = new Intent(context, MainActivity.class);
+        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+
+                .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentIntent(pendingIntent)
+                .setContentText("Vergeet uw oefeningen niet!")
+                .setContentTitle("Fyxio")
+                .setAutoCancel(true);
+        notificationManager.notify(100, builder.build());
+
+
+    }
+}
--- Fyxio/app/src/main/java/ipmedt4/example/com/fyxio/Settings.java
+++ Fyxio/app/src/main/java/ipmedt4/example/com/fyxio/Settings.java
@@ -6,6 +6,142 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Wendy on 13-6-2017.
  */
 
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Random;
+
 public class Settings extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pending_intent;
+
+    private TimePicker alarmTimePicker;
+    private TextView alarmTextView;
+
+    private AlarmReceiver alarm;
+
+
+    Settings inst;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+
+        this.context = this;
+
+        //alarm = new AlarmReceiver();
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+        final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
+
+        // Get the alarm manager service
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // set the alarm to the time that you picked
+        final Calendar calendar = Calendar.getInstance();
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTimePicker.setIs24HourView(true);
+
+
+        Button start_alarm= (Button) findViewById(R.id.start_alarm);
+        start_alarm.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+
+            @Override
+            public void onClick(View v) {
+
+                calendar.add(Calendar.SECOND, 3);
+                //setAlarmText("You clicked a button");
+
+                final int hour = alarmTimePicker.getCurrentHour();
+                final int minute = alarmTimePicker.getCurrentMinute();;
+
+                Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+                setAlarmText("You clicked a " + hour + " and " + minute);
+
+
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+                myIntent.putExtra("extra", "yes");
+                pending_intent = PendingIntent.getBroadcast(Settings.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+                alarmManager.setRepeating(alarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24*60*60*1000 , pending_intent);
+
+                // now you should change the set Alarm text so it says something nice
+
+
+                setAlarmText("Uw dagelijkse reminder is ingesteld op " + hour + ":" + minute);
+                Toast.makeText(getApplicationContext(), "U hebt de reminder ingesteld", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        Button stop_alarm= (Button) findViewById(R.id.stop_alarm);
+        stop_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                int min = 1;
+                int max = 9;
+
+                Random r = new Random();
+                int random_number = r.nextInt(max - min + 1) + min;
+                Log.e("random number is ", String.valueOf(random_number));
+
+                myIntent.putExtra("extra", "no");
+                sendBroadcast(myIntent);
+
+                alarmManager.cancel(pending_intent);
+                setAlarmText("Reminder is geannuleerd");
+                //setAlarmText("You clicked a " + " canceled");
+            }
+        });
+
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.e("MyActivity", "on Destroy");
+    }
+
 
 }
